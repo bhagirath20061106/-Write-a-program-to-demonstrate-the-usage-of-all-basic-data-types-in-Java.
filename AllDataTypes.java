2be68a8bda910e5e3b06226debc9f2c9106a0cd1
pasupulabhagirathprabas <pbalu5077@gmail.com>
2025-01-30 06:54:43 +0530

    Add files via upload

---
+++ AllDataTypes.java
@@ -0,0 +1,16 @@
+package bhagirath;
+import java.util.*;
+public class AllDataTypes {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		float f=sc.nextFloat();
+		double d=sc.nextDouble();
+		System.out.println();
+System.out.println(n);
+System.out.println(f);
+System.out.println(d);
+	}
+
+}
